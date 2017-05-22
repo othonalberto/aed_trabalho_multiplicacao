e5d86c899651b1300723276b0b936735201d3568
bwpedro <pwarmling0@gmail.com>
2017-05-22 18:09:34 -0300

    add insereInicio

--- biblioteca.h
+++ biblioteca.h
@@ -13,9 +13,12 @@ typedef struct TCabeca{
 
 
 /*	Função para criar a cabeça da lista.
-	implementada por Pedro Warmling Botelho	*/
+	Implementada por Pedro Warmling Botelho	*/
 TCabeca *criaCabeca();
 
+/*	Função para inserer no inicio da lista.
+	Implementada por Pedro Warmling Botelho	*/
+TNo *insereInicio(TCabeca *cabeca, char k);
 
 /* Função para inserir no fim da lista.
    Implementada por Othon Alberto */
