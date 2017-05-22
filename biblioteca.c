73e656551cc7fd506099d7e2c0d8001c760c5e03
bwpedro <pwarmling0@gmail.com>
2017-05-22 16:13:39 -0300

    add cria cabeca

--- biblioteca.c
+++ biblioteca.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "biblioteca.h"
+
+TCabeca *criaCabeca(){
+    TCabeca *cabeca = (TCabeca *) malloc(sizeof(TCabeca));
+    if (cabeca == NULL)
+        return NULL;
+
+    cabeca->prim = NULL;
+    cabeca->ult = NULL;
+
+    return cabeca;
+}
